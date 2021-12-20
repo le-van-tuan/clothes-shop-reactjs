457132921c7d444e08b2793626c2f0525ac281c3
Le Van Tuan <levantuan.itvn@gmail.com>
2021-12-20 20:27:22 +0700

    Product details

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -3,13 +3,15 @@ import {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
 import {useDispatch} from "react-redux";
 import {getProductDetail} from "../redux/apiCalls";
-import {Button, Divider, InputNumber, Typography} from "antd";
+import {Button, Divider, InputNumber, Select, Typography} from "antd";
 import {BASE_URL} from "../helpers/axiosInstance";
 import {ShoppingCartOutlined} from "@ant-design/icons";
 import {FavoriteBorderOutlined} from "@material-ui/icons";
 import {getReadableSpecifications} from "../helpers/utils";
+import SimpleImageSlider from "react-simple-image-slider";
 
 const {Text} = Typography;
+const {Option} = Select;
 
 const Container = styled.div`
   display: flex;
@@ -72,8 +74,23 @@ const QuantityInput = styled.div`
 const SpecRow = styled.div`
   display: flex;
   flex-direction: row;
+
   &:not(:first-child) {
     margin-top: 5px;
+  }
+`
+
+const TierWrapper = styled.div`
+  margin-top: 20px;
+  display: flex;
+  align-items: center;
+
+  & > div {
+    width: 80px;
+  }
+
+  &:not(:first-child) {
+    margin-top: 15px;
   }
 `
 
@@ -96,19 +113,45 @@ const ProductDetails = () => {
         return BASE_URL + "products/images/" + thumbnail.url;
     }
 
+    const images = [
+        {url: getDefaultThumbnail()},
+    ];
+
     return (
         <Container>
             <Content>
                 <ProductWrapper>
                     <ProductImages>
-                        <div className={"ImgContainer"}>
-                            <img style={{width: "100%"}} alt="example"
-                                 src={getDefaultThumbnail()}/>
-                        </div>
+                        <SimpleImageSlider
+                            width={450}
+                            height={450}
+                            images={images}
+                            showBullets={true}
+                            showNavs={true}
+                        />
                     </ProductImages>
                     <ProductInfo>
                         <h2>{product.name}</h2>
                         <Text style={{color: "#ee4d2d", fontSize: 18}} strong>$200</Text>
+                        {
+                            [].concat(product['tierVariations']).map((tier) => {
+                                if (!tier) return null;
+                                return (
+                                    <TierWrapper>
+                                        <div>{tier.name}</div>
+                                        <Select style={{minWidth: 100, marginLeft: 20}}>
+                                            {[].concat(tier.options).map((option) => {
+                                                return (
+                                                    <Option value={option.id}>
+                                                        {option.value}
+                                                    </Option>
+                                                )
+                                            })}
+                                        </Select>
+                                    </TierWrapper>
+                                )
+                            })
+                        }
                         <QuantityInput>
                             <Text type="secondary">Quantity</Text>
                             <div>
